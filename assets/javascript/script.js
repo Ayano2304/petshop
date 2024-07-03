4c05acf9011a5b4318a2cb20322b98aec4629dce
Ayanoooo <marthawij23@gmail.com>
2024-07-03 10:45:14 +0700

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,39 @@
+function activateLink(element) {
+  // Menghapus kelas 'active' dari semua link
+  document.querySelectorAll(".nav-link").forEach(function (link) {
+    link.classList.remove("active");
+  });
+
+  // Menambahkan kelas 'active' ke link yang diklik
+  element.classList.add("active");
+
+  // Menutup navbar jika dalam mode collapse (untuk layar kecil)
+  if ($(".navbar-toggler").is(":visible")) {
+    $(".navbar-collapse").collapse("hide");
+  }
+}
+
+function showInfo(category) {
+  // Sembunyikan semua info container
+  document.querySelectorAll(".info-container").forEach(function (info) {
+    info.style.display = "none";
+  });
+
+  // Tampilkan info container yang dipilih
+  document.getElementById(category + "-info").style.display = "block";
+}
+
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+const navbar = document.querySelector(".navbar");
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 50) {
+    // Adjust the value as needed
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
